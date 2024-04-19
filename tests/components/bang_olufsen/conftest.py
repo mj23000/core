8e41cbf773fdffb999f73bfec6328897b32f7ffa
mj23000 <markusjacobsen@hotmail.com>
2024-04-19 15:40:02 +0200

    Add source testing

--- tests/components/bang_olufsen/conftest.py
+++ tests/components/bang_olufsen/conftest.py
@@ -3,7 +3,15 @@
 from collections.abc import Generator
 from unittest.mock import AsyncMock, patch
 
-from mozart_api.models import BeolinkPeer, Source, SourceArray
+from mozart_api.models import (
+    Action,
+    BeolinkPeer,
+    ContentItem,
+    RemoteMenuItem,
+    Source,
+    SourceArray,
+    SourceTypeEnum,
+)
 import pytest
 
 from homeassistant.components.bang_olufsen.const import DOMAIN
@@ -55,18 +63,21 @@ def mock_mozart_client() -> Generator[AsyncMock, None, None]:
         client.get_available_sources = AsyncMock()
         client.get_available_sources.return_value = SourceArray(
             items=[
+                # Is in the HIDDEN_SOURCE_IDS constant, so should not be user selectable
                 Source(
                     name="AirPlay",
                     id="airPlay",
                     is_enabled=True,
                     is_multiroom_available=False,
                 ),
+                # The only available source
                 Source(
                     name="Tidal Connect",
                     id="tidalConnect",
                     is_enabled=True,
                     is_multiroom_available=True,
                 ),
+                # Is disabled, so should not be user selectable
                 Source(
                     name="Powerlink",
                     id="pl",
@@ -74,6 +85,118 @@ def mock_mozart_client() -> Generator[AsyncMock, None, None]:
                 ),
             ]
         )
+        client.get_remote_menu = AsyncMock()
+        client.get_remote_menu.return_value = {
+            # Music category, so shouldn't be included in video sources
+            "b355888b-2cde-5f94-8592-d47b71d52a27": RemoteMenuItem(
+                action_list=[
+                    Action(
+                        button_name=None,
+                        content_id="netRadio://6629967157728971",
+                        deezer_user_id=None,
+                        gain_db=None,
+                        listening_mode_id=None,
+                        preset_key=None,
+                        queue_item=None,
+                        queue_settings=None,
+                        radio_station_id=None,
+                        source=None,
+                        speaker_group_id=None,
+                        stand_position=None,
+                        stop_duration=None,
+                        tone_name=None,
+                        type="triggerContent",
+                        volume_level=None,
+                    )
+                ],
+                scene_list=None,
+                disabled=None,
+                dynamic_list=None,
+                first_child_menu_item_id=None,
+                label="Yle Radio Suomi Helsinki",
+                next_sibling_menu_item_id="0b4552f8-7ac6-5046-9d44-5410a815b8d6",
+                parent_menu_item_id="eee0c2d0-2b3a-4899-a708-658475c38926",
+                available=None,
+                content=ContentItem(
+                    categories=["music"],
+                    content_uri="netRadio://6629967157728971",
+                    label="Yle Radio Suomi Helsinki",
+                    source=SourceTypeEnum(value="netRadio"),
+                ),
+                fixed=True,
+                id="b355888b-2cde-5f94-8592-d47b71d52a27",
+            ),
+            # Has "hdmi" as category, so be included in video sources
+            "b6591565-80f4-4356-bcd9-c92ca247f0a9": RemoteMenuItem(
+                action_list=[
+                    Action(
+                        button_name=None,
+                        content_id="tv://hdmi_1",
+                        deezer_user_id=None,
+                        gain_db=None,
+                        listening_mode_id=None,
+                        preset_key=None,
+                        queue_item=None,
+                        queue_settings=None,
+                        radio_station_id=None,
+                        source=None,
+                        speaker_group_id=None,
+                        stand_position=None,
+                        stop_duration=None,
+                        tone_name=None,
+                        type="triggerContent",
+                        volume_level=None,
+                    )
+                ],
+                scene_list=None,
+                disabled=False,
+                dynamic_list="none",
+                first_child_menu_item_id=None,
+                label="HDMI A",
+                next_sibling_menu_item_id="0ba98974-7b1f-40dc-bc48-fbacbb0f1793",
+                parent_menu_item_id="b66c835b-6b98-4400-8f84-6348043792c7",
+                available=True,
+                content=ContentItem(
+                    categories=["hdmi"],
+                    content_uri="tv://hdmi_1",
+                    label="HDMI A",
+                    source=SourceTypeEnum(value="tv"),
+                ),
+                fixed=False,
+                id="b6591565-80f4-4356-bcd9-c92ca247f0a9",
+            ),
+            # The parent remote menu item. Has the TV label and should therefore not be included in video sources
+            "b66c835b-6b98-4400-8f84-6348043792c7": RemoteMenuItem(
+                action_list=[],
+                scene_list=None,
+                disabled=False,
+                dynamic_list="none",
+                first_child_menu_item_id="b6591565-80f4-4356-bcd9-c92ca247f0a9",
+                label="TV",
+                next_sibling_menu_item_id="0c4547fe-d3cc-4348-a425-473595b8c9fb",
+                parent_menu_item_id=None,
+                available=True,
+                content=None,
+                fixed=True,
+                id="b66c835b-6b98-4400-8f84-6348043792c7",
+            ),
+            # Has an empty content, so should not be included
+            "64c9da45-3682-44a4-8030-09ed3ef44160": RemoteMenuItem(
+                action_list=[],
+                scene_list=None,
+                disabled=False,
+                dynamic_list="none",
+                first_child_menu_item_id=None,
+                label="ListeningPosition",
+                next_sibling_menu_item_id=None,
+                parent_menu_item_id="0c4547fe-d3cc-4348-a425-473595b8c9fb",
+                available=True,
+                content=None,
+                fixed=True,
+                id="64c9da45-3682-44a4-8030-09ed3ef44160",
+            ),
+        }
+
         yield client
 
 
--- tests/components/bang_olufsen/test_media_player.py
+++ tests/components/bang_olufsen/test_media_player.py
@@ -1,13 +1,73 @@
 """Test the Bang & Olufsen media_player entity."""
 
+from homeassistant.components.bang_olufsen.const import FALLBACK_SOURCES
+from homeassistant.components.bang_olufsen.media_player import BangOlufsenMediaPlayer
 from homeassistant.core import HomeAssistant
 
 # pytestmark = pytest.mark.usefixtures("mock_setup_entry")
 
 
-def test_initialize(hass: HomeAssistant, mock_mozart_client) -> None:
-    """Test the integration is initialized properly in _initialize."""
+# async def test_initialization(
+#     hass: HomeAssistant, mock_mozart_client, mock_config_entry
+# ) -> None:
+#     """Test the integration is initialized properly in _initialize, async_added_to_hass and __init__."""
+#     mock_config_entry.add_to_hass(hass)
+#     await hass.config_entries.async_setup(mock_config_entry.entry_id)
+#     await hass.async_block_till_done()
+
+#     media_player = BangOlufsenMediaPlayer(mock_config_entry, mock_mozart_client)
 
 
-def test_update_sources(hass: HomeAssistant, mock_mozart_client) -> None:
+async def test_update_sources(
+    hass: HomeAssistant, mock_mozart_client, mock_config_entry
+) -> None:
     """Test sources are correctly handled in _update_sources."""
+    media_player = BangOlufsenMediaPlayer(mock_config_entry, mock_mozart_client)
+
+    assert not media_player._sources
+    assert not media_player._audio_sources
+    assert not media_player._video_sources
+
+    await media_player._update_sources()
+
+    assert len(media_player._video_sources) == 1
+    assert len(media_player._audio_sources) == 1
+    assert len(media_player._sources) == (1 + 1)
+
+
+async def test_update_sources_audio_only(
+    hass: HomeAssistant, mock_mozart_client, mock_config_entry
+) -> None:
+    """Test sources are correctly handled in _update_sources."""
+    mock_mozart_client.get_remote_menu.return_value = {}
+
+    media_player = BangOlufsenMediaPlayer(mock_config_entry, mock_mozart_client)
+
+    assert not media_player._sources
+    assert not media_player._audio_sources
+    assert not media_player._video_sources
+
+    await media_player._update_sources()
+
+    assert not media_player._video_sources
+    assert len(media_player._audio_sources) == 1
+    assert len(media_player._sources) == (0 + 1)
+
+
+async def test_update_sources_outdated_api(
+    hass: HomeAssistant, mock_mozart_client, mock_config_entry
+) -> None:
+    """Test fallback sources are correctly handled in _update_sources."""
+    mock_mozart_client.get_available_sources.side_effect = ValueError()
+
+    media_player = BangOlufsenMediaPlayer(mock_config_entry, mock_mozart_client)
+
+    assert not media_player._sources
+    assert not media_player._audio_sources
+    assert not media_player._video_sources
+
+    await media_player._update_sources()
+
+    assert not media_player._video_sources
+    assert len(media_player._audio_sources) == len(FALLBACK_SOURCES)
+    assert len(media_player._sources) == len(FALLBACK_SOURCES)
