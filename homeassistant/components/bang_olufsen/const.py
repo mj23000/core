f0bcc7c39338ffad8e463bfa75388f50af89ca6b
mj23000 <markusjacobsen@hotmail.com>
2025-05-09 14:08:05 +0200

    Add value content option

--- homeassistant/components/bang_olufsen/const.py
+++ homeassistant/components/bang_olufsen/const.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from enum import StrEnum
-from typing import Final
+from typing import Final, TypedDict
 
 from mozart_api.models import Source, SourceTypeEnum
 
@@ -140,11 +140,13 @@ CONF_SERIAL_NUMBER: Final = "serial_number"
 CONF_PAGE_TITLE: Final = "page_title"
 CONF_PAGES: Final = "pages"
 CONF_TEXT: Final = "text"
+CONF_VALUE: Final = "value"
 CONF_HALO: Final = "halo"
 CONF_ENTITY_MAP: Final = "entity_map"
 CONF_TITLE: Final = "title"
 CONF_SUBTITLE: Final = "subtitle"
 CONF_DEFAULT_BUTTON: Final = "default_button"
+CONF_CONTENT: Final = "content"
 # Menu options / step IDs
 HALO_OPTION_PAGE = "page"
 HALO_OPTION_MODIFY_PAGE = "modify_page"
@@ -158,6 +160,15 @@ HALO_BUTTON_ICONS: list[str] = [icon.name for icon in Icons]
 
 # Timeout for sending wheel events in seconds
 HALO_WHEEL_TIMEOUT: Final = 0.125
+
+
+class EntityMapValues(TypedDict):
+    """TypedDict for Halo button information and settings."""
+
+    entity_id: str
+    # Use entity value instead of Icon / Text
+    value: bool
+
 
 # Mozart models
 MOZART_MODELS: Final[list[BangOlufsenModel]] = [
